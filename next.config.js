695ea536700a841603acad71565d38a700caff87
gicic95 <milangicic95@outlook.com>
2023-05-08 15:38:41 +0200

    Config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-const path = require("path");
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     unoptimized: true,
-    domains: ["api.beta.jakov.rs"],
+    domains: ['api.beta.jakov.rs'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.beta.jakov.rs',
+      },
+    ],
   },
 };
 
